6ad9674c4af5fd09a2a362a95c9d6f2eca8b80bd
Freddy Rincones <freddyrincones@gmail.com>
2025-04-08 13:47:51 -0500

    Nuevo Modulo de Credito

---
+++ src/app/(apps layout)/apps/credits/credit-list/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import CreditListBody from './CreditListBody';
+import CreditAppHeader from './CreditAppHeader';
+
+const CreditList = () => {
+    return (
+        <>
+            <CreditAppHeader />
+            <CreditListBody />
+        </>
+    );
+}
+
+export default CreditList;
